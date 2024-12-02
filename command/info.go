4dc7d6697aab81a77311324fd9a4542157ee6b75
ezTxmMC <eztxmmc@hotmail.com>
2024-12-02 10:27:54 +0100

    started with the info command

--- command/info.go
+++ command/info.go
@@ -1,7 +1,17 @@
 package command
 
-import "fmt"
+import (
+	"fmt"
+
+	"smoothcloudcli/json"
+)
 
 func Info() {
-	fmt.Println("Infos")
+	var result map[string]interface{}
+	json.LoadJSON("cloud/config.json", result)
+	fmt.Println("-----------------Information-----------------")
+	fmt.Println("SmoothCloud v1.0-indev")
+	fmt.Println("xxxxxx@dev")
+	fmt.Printf("Memory: %s\n", result)
+	fmt.Println("-----------------Information-----------------")
 }
