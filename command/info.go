a59a9649f6a5eaf18d09fa943fd4698430d4b66c
ezTxmMC <eztxmmc@hotmail.com>
2024-12-11 14:25:54 +0100

    removed standalone stuff, created custom cli help and started preparing for http client development

--- command/info.go
+++ command/info.go
@@ -2,16 +2,12 @@ package command
 
 import (
 	"fmt"
-
-	"smoothcloudcli/json"
 )
 
 func Info() {
-	var result map[string]interface{}
-	json.LoadJSON("cloud/config.json", result)
 	fmt.Println("-----------------Information-----------------")
 	fmt.Println("SmoothCloud v1.0-indev")
 	fmt.Println("xxxxxx@dev")
-	fmt.Printf("Memory: %s\n", result)
+	fmt.Println("Memory:")
 	fmt.Println("-----------------Information-----------------")
 }
--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/spf13/cobra"
 
 	"smoothcloudcli/command"
-	"smoothcloudcli/command/group"
-	"smoothcloudcli/json"
 )
 
 func main() {
@@ -16,11 +14,14 @@ func main() {
 		Use:     "smoothcloud",
 		Aliases: []string{"sc"},
 	}
+	var createCommand = &cobra.Command{
+		Use:   "create",
+	}
 	var createGroupCommand = &cobra.Command{
-		Use:   "creategroup",
-		Short: "Creates a group",
+		Use:   "group",
+		Short: "Create a group with a configurator",
 		Run: func(cmd *cobra.Command, args []string) {
-			group.CreateGroup()
+			
 		},
 	}
 	var infoCommand = &cobra.Command{
@@ -30,42 +31,27 @@ func main() {
 			command.Info()
 		},
 	}
-	var installCommand = &cobra.Command{
-		Use:   "install",
+	var setupCommand = &cobra.Command{
+		Use:   "setup",
 		Short: "Will install the cloud",
 		Args:  cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			command.Install()
+			command.Setup()
 		},
 	}
-	type Test struct {
-		Object	 interface{}	`json:"object"`
-	}
+	createCommand.AddCommand(createGroupCommand)
+	rootCommand.AddCommand(createCommand, infoCommand, setupCommand)
+	cobra.OnInitialize(func() {
+		cobra.EnableCommandSorting = false
+	})
+	rootCommand.CompletionOptions.DisableDefaultCmd = true
+	rootCommand.SetHelpTemplate(`
+{{.UseLine}}
 
-	type Test2 struct {
-		String 	 string	`json:"string"`
-	}
-	var testCommand = &cobra.Command{
-		Use: "test",
-		Short: "Testing things",
-		Args: cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			var subTestCommand = &args[0]
-			switch *subTestCommand {
-			case "jsonsave":
-				json.SaveJSON("test.json", Test{Object: Test{Object: nil}})
-			case "jsonupdate":
-				json.UpdateKeyValue("test.json", "object", Test{Object: make(map[string]string)})
-			case "jsonadd":
-				json.AddKeyValue("test.json", "string", Test2{String: "Test"})
-			case "jsonaddnested":
-				json.AddNestedKeyValue("test.json", "test.string", Test2{String: "Nested Test"})
-			case "jsonremove":
-				json.RemoveKey("test.json", "object")
-			}
-		},
-	}
-	rootCommand.AddCommand(createGroupCommand, infoCommand, installCommand, testCommand)
+Available Commands:
+{{range .Commands}}{{if .IsAvailableCommand}}{{.Name | printf "%-15s"}}{{.Short}}{{end}}
+{{range .Commands}}{{if .IsAvailableCommand}}{{printf "→ "}}{{.Name | printf "%-13s"}}{{.Short}}{{end}}{{end}}{{end}}
+`)
 	if err := rootCommand.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
